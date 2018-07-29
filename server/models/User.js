9de89227269bcbc138dd561db94b3da7285e4934
Luis <luis.gilgb@gmail.com>
2018-07-29 20:34:15 +0200

    First steps for a User model

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create User Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('User', UserSchema);

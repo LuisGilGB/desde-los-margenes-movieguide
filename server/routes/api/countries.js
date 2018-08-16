cc5741ba612f50a41a38735e157ed578237c50df
Luis <luis.gilgb@gmail.com>
2018-08-16 20:42:41 +0200

    Get countries service defined

--- server/routes/api/countries.js
+++ server/routes/api/countries.js
@@ -3,9 +3,27 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Import models
+const modelsDir = '../../models/';
+const Country = require(`${modelsDir}Country`);
+
 // @route   GET api/countries/test
 // @desc    Tests countries route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Countries works'}));
+
+// @route   GET api/countries/test
+// @desc    Tests countries route
+// @access  Public
+router.get('/', (req, res) => {
+    Country.find()
+        .then(countries => {
+            if (!countries) {
+                return res.status(404).json({ countries: 'There are no countries' });
+            }
+            res.json(countries);
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;

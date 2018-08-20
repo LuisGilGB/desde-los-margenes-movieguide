80456db8cdadd9fd6e3ca3cf7128090c56c5e87a
Luis <luis.gilgb@gmail.com>
2018-08-20 20:53:59 +0200

    Passport fix for movies router

--- server/routes/api/movies.js
+++ server/routes/api/movies.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 // Import models
 const modelsDir = '../../models/';

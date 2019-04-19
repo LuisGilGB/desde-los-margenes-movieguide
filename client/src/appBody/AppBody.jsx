22a0a0626735894df4efcb078c375503254935a0
LuisGilGB <luis.gilgb@gmail.com>
2019-04-19 02:08:27 +0200

    Removed register and login temp views

--- client/src/appBody/AppBody.jsx
+++ client/src/appBody/AppBody.jsx
@@ -15,7 +15,7 @@ const AppBody = props => (
         <Switch>
             <Route path={ROUTES.MOVIES.CATALOG} component={MoviesContainer} />
             <Route path={ROUTES.PEOPLE} component={PeopleContainer} />
-            <Route path={ROUTES.REGISTER} exact render={() => (
+            {/*<Route path={ROUTES.REGISTER} exact render={() => (
                 <Container layout="center" className="app-register">
                     This would be the place to register.
                 </Container>
@@ -24,7 +24,7 @@ const AppBody = props => (
                 <Container layout="center" className="app-login">
                     This would be the place to log in.
                 </Container>
-            )} />
+            )} />*/}
             <Route path={ROUTES.ROOT} component={LandingPage} />
         </Switch>
     </Container>

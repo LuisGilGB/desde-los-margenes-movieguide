61af4a795c4c0d81e47312725f772566d5d2eba1
LuisGilGB <luis.gilgb@gmail.com>
2019-08-23 19:00:32 +0200

    Get people endpoint ready at BFF

--- server/src/routes/bff-api/people.js
+++ server/src/routes/bff-api/people.js
@@ -14,11 +14,35 @@ const Person = require(`${modelsDir}Person`);
 // Import validators
 const validatePersonToAdd = require('../../validation/person/addPerson.js');
 
-// @route   GET api/people/test
+// @route   GET bff/people/test
 // @desc    Tests people route
 // @access  Public
 router.get('/test', (req, res) => peopleBusinessLogic.test(req)
     .then(answer => res.json(answer))
     .catch(err => console.log(err)));
+
+// @route   GET bff/people
+// @desc    Get all people into the database
+// @access  Public
+router.get('/', (req, res) => {
+    peopleBusinessLogic.getPeople(req)
+        .then(people => {
+            const errors = {}
+            if (!people) {
+                errors.people = 'There are no people';
+                return res.status(404).json(errors);
+            }
+            const peopleForBFF = people.map(p => ({
+                id: p.uniqueAliasSlug,
+                name: p.name,
+                description: p.name,
+                pic: p.pic,
+                countries: p.countries,
+                movies: p.movies
+            }));
+            res.json(peopleForBFF);
+        })
+        .catch(err => console.log(err));
+});
 
 module.exports = router;

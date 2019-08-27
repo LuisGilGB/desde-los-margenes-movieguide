34b44c3c82824008d64a518e955bee3c537ebb82
LuisGilGB <luis.gilgb@gmail.com>
2019-08-27 19:53:34 +0200

    Function getPersonSchemaAttributes ready

--- server/src/models/person/functions.js
+++ server/src/models/person/functions.js
@@ -1,4 +1,9 @@
-const getPersonSchemaAttributes = (obj = {}) => ({});
+const {KEYS} = './consts.js';
+
+const getPersonSchemaAttributes = (obj = {}) => KEYS.reduce((obj0, key) => object[key] !== undefined ? {
+    ...obj0,
+    [key]: object[key]
+} : obj0, {});
 
 module.exports = {
     getPersonSchemaAttributes

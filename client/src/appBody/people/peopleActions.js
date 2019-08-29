647c883496f8704e5fb77270927c9b9678c10002
LuisGilGB <luis.gilgb@gmail.com>
2019-08-29 21:06:33 +0200

    Defined people load callback actions

--- client/src/appBody/people/peopleActions.js
+++ client/src/appBody/people/peopleActions.js
@@ -1,10 +1,20 @@
 export const actions = {
-    LOAD_PEOPLE: 'LOAD_PEOPLE'
+    LOAD_PEOPLE: 'LOAD_PEOPLE',
+    LOAD_PEOPLE_DONE: 'LOAD_PEOPLE_DONE',
+    LOAD_PEOPLE_FAILED: 'LOAD_PEOPLE_FAILED'
 }
 
 export const actionCreators = {
     loadPeople: () => ({
         type: actions.LOAD_PEOPLE,
+        payload: {}
+    }),
+    loadPeopleDone: (data) => ({
+        type: actions.LOAD_PEOPLE_DONE,
+        payload: {data}
+    }),
+    loadPeopleFailed: () => ({
+        type: actions.LOAD_PEOPLE_FAILED,
         payload: {}
     })
 }

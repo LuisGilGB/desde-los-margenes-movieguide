0d86e19fb6200be5d351f52383d7d1c1184f3e3b
LuisGilGB <luis.gilgb@gmail.com>
2019-09-02 21:16:41 +0200

    PeopleListView remade as a functional component

--- client/src/appBody/people/list/PeopleListView.jsx
+++ client/src/appBody/people/list/PeopleListView.jsx
@@ -1,35 +1,31 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Container} from '../../../common';
 import PeopleList from './PeopleList';
 
-class PeopleListView extends Component {
-    componentDidMount () {
-        const {loadPeople} = this.props;
+const PeopleListView = props => {
+    const {
+        people = [],
+        loadPeople,
+        goToPersonDetail,
+        ...otherProps
+    } = props;
 
+    useEffect(() => {
         loadPeople && loadPeople();
-    }
+    }, [])
 
-    render () {
-        const {
-            people = [],
-            loadPeople,
-            goToPersonDetail,
-            ...otherProps
-        } = this.props;
-
-        return (
-            <Container
-                {...otherProps}
-                layout="fit"
-                className="people-list-view"
-            >
-                <PeopleList
-                    data={people}
-                    onItemClick={goToPersonDetail}
-                />
-            </Container>
-        );
-    }
+    return (
+        <Container
+            {...otherProps}
+            layout="fit"
+            className="people-list-view"
+        >
+            <PeopleList
+                data={people}
+                onItemClick={goToPersonDetail}
+            />
+        </Container>
+    );
 }
 
 export default PeopleListView;

26f1c121df4740e4231960290816decdbba65b4d
LuisGilGB <luis.gilgb@gmail.com>
2019-09-03 19:59:02 +0200

    Load person data redux actions

--- client/src/appBody/people/peopleActions.js
+++ client/src/appBody/people/peopleActions.js
@@ -1,7 +1,11 @@
 export const actions = {
     LOAD_PEOPLE: 'LOAD_PEOPLE',
     LOAD_PEOPLE_DONE: 'LOAD_PEOPLE_DONE',
-    LOAD_PEOPLE_FAILED: 'LOAD_PEOPLE_FAILED'
+    LOAD_PEOPLE_FAILED: 'LOAD_PEOPLE_FAILED',
+
+    LOAD_PERSON_DATA: 'LOAD_PERSON_DATA',
+    LOAD_PERSON_DATA_DONE: 'LOAD_PERSON_DATA_DONE',
+    LOAD_PERSON_DATA_FAILED: 'LOAD_PERSON_DATA_FAILED'
 }
 
 export const actionCreators = {
@@ -15,6 +19,19 @@ export const actionCreators = {
     }),
     loadPeopleFailed: () => ({
         type: actions.LOAD_PEOPLE_FAILED,
+        payload: {}
+    }),
+
+    loadPersonData: (personId) => ({
+        type: actions.LOAD_PERSON_DATA,
+        payload: {personId}
+    }),
+    loadPersonDataDone: (data) => ({
+        type: actions.LOAD_PERSON_DATA_DONE,
+        payload: {data}
+    }),
+    loadPersonDataFailed: () => ({
+        type: actions.LOAD_PERSON_DATA_FAILED,
         payload: {}
     })
 }

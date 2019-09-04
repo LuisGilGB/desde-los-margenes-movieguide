366af8ec9ab1542f77d02803261c751753d5371c
LuisGilGB <luis.gilgb@gmail.com>
2019-09-04 18:39:18 +0200

    Admin-client log in reducer

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+    isLoggedIn: false,
+    logInIsFetching: false,
+    userName: '',
+    userPass: '',
+    token: ''
+}
+
+const actions = {
+    FETCH_LOG_IN        : 'FETCH_LOG_IN',
+    FETCH_LOG_IN_DONE   : 'FETCH_LOG_IN_DONE',
+    FETCH_LOG_IN_FAILED : 'FETCH_LOG_IN_FAILED',
+    LOG_OUT             : 'LOG_OUT'
+}
+
+const loginReducer = (state = initialState, action) => {
+    const {type, payload = {}} = action;
+    const reducers = {
+        [actions.FETCH_LOG_IN]: () => ({
+            ...state,
+            isLoggedIn: false,
+            logInIsFetching: true,
+            token: initialState.token
+        }),
+        [actions.FETCH_LOG_IN_DONE]: () => ({
+            ...state,
+            isLoggedIn: true,
+            logInIsFetching: false,
+            userPass: initialState.userPass,
+            token: payload.token
+        }),
+        [actions.FETCH_LOG_IN_FAILED]: () => ({
+            ...state,
+            isLoggedIn: false,
+            logInIsFetching: false
+        }),
+        [actions.LOG_OUT]: () => ({
+            ...state,
+            ...initialState
+        })
+    }
+
+    return reducers[type] ? reducers[type]() : state;
+}
+
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+        </div>
+    );
 }
 
 export default App;

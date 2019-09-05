9ed1def8a6d0457a27366dfee905f5a2d3fca867
LuisGilGB <luis.gilgb@gmail.com>
2019-09-05 21:22:15 +0200

    Added React Router

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -1,52 +1,58 @@
 import React from 'react';
+import {Router} from 'react-router';
+import {createBrowserHistory} from "history";
 import LogInManager from './LogInManager';
 import AppHeader from './appHeader/AppHeader';
 import {UserConsumer} from './UserContext';
 import './App.css';
 
+const history = createBrowserHistory();
+
 const App = props => {
     const goToHome = () => console.log('Go to home');
     return (
-        <LogInManager>
-            {(logInManagerProps) => {
-                const {
-                    isLoggedIn,
-                    logInIsFetching,
-                    userMail,
-                    userPass,
-                    logIn,
-                    logOut,
-                    onUserMailChange,
-                    onUserPassChange
-                } = logInManagerProps;
+        <Router history={history}>
+            <LogInManager>
+                {(logInManagerProps) => {
+                    const {
+                        isLoggedIn,
+                        logInIsFetching,
+                        userMail,
+                        userPass,
+                        logIn,
+                        logOut,
+                        onUserMailChange,
+                        onUserPassChange
+                    } = logInManagerProps;
 
-                return (
-                    <div className="App">
-                        <AppHeader
-                            goToHome={goToHome}
-                            logInIsFetching={logInIsFetching}
-                            userMail={userMail}
-                            userPass={userPass}
-                            logIn={logIn}
-                            logOut={logOut}
-                            onUserMailChange={onUserMailChange}
-                            onUserPassChange={onUserPassChange}
-                        />
-                        {isLoggedIn ? (
-                            <UserConsumer>
-                                {userProps => (<div>
-                                    User {userProps.currentUser} is logged in with token {userProps.token}
-                                </div>)}
-                            </UserConsumer>
-                        ) : (
-                            <div>
-                                Hola
-                            </div>
-                        )}
-                    </div>
-                )}
-            }
-        </LogInManager>
+                    return (
+                        <div className="App">
+                            <AppHeader
+                                goToHome={goToHome}
+                                logInIsFetching={logInIsFetching}
+                                userMail={userMail}
+                                userPass={userPass}
+                                logIn={logIn}
+                                logOut={logOut}
+                                onUserMailChange={onUserMailChange}
+                                onUserPassChange={onUserPassChange}
+                            />
+                            {isLoggedIn ? (
+                                <UserConsumer>
+                                    {userProps => (<div>
+                                        User {userProps.currentUser} is logged in with token {userProps.token}
+                                    </div>)}
+                                </UserConsumer>
+                            ) : (
+                                <div>
+                                    Hola
+                                </div>
+                            )}
+                        </div>
+                    )}
+                }
+            </LogInManager>
+        </Router>
     );
 }
 

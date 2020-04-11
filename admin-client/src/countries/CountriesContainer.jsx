dd73c201dcf475a482edada9e6d302b1c2ece445
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2020-04-11 17:33:05 +0200

    Use of imported Grid to show countries

--- admin-client/src/countries/CountriesContainer.jsx
+++ admin-client/src/countries/CountriesContainer.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Container from '@luisgilgb/react-container';
+import Grid from '@luisgilgb/react-grid';
 import axios from 'axios';
 import ROUTES from '../routes';
 
@@ -28,8 +29,29 @@ const CountriesContainer = props => {
             >
                 Countries
             </Container>
-            <Container>
-                {countries.map(c => <div key={`country-${c.countryId}`}>{(c && c.name && c.name.es) || ''}</div>)}
+            <Container
+                layout="fit"
+            >
+                <Grid
+                    data={countries}
+                    columns={[{
+                        columnKey: 'countryId',
+                        title: 'Country ID',
+                        width: 120
+                    },
+                    {
+                        columnKey: 'shortName',
+                        title: 'Country short name',
+                        width: 180
+                    },
+                    {
+                        columnKey: 'name',
+                        title: 'Country name',
+                        minWidth: 300,
+                        flex: 1,
+                        renderFn: (value) => value.en
+                    }]}
+                />
             </Container>
         </Container>
     );

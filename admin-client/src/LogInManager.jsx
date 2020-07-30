0247f09728cf97f23196b62c53fba36f9e1be35a
LuisGilGB <luis.gilgb@gmail.com>
2020-07-30 21:13:32 +0200

    User login persistence and added code formatting with prettier

--- admin-client/src/LogInManager.jsx
+++ admin-client/src/LogInManager.jsx
@@ -1,137 +1,201 @@
-import React, {useReducer} from 'react';
+import React, { useReducer } from 'react';
 import axios from 'axios';
-import {UserProvider} from './UserContext';
+import { UserProvider } from './UserContext';
+
+const LOCAL_STORAGE_KEY = 'desdeLosMargenesApp';
+const USER_KEY = 'user';
+const USER_TOKEN_KEY = 'userToken';
+
+const parseLocalStorage = () =>
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+
+const loadFromLocalStorage = (key) => {
+  const { [key]: res } = parseLocalStorage();
+  return res;
+};
+
+const saveInLocalStorageAs = (key, value) => {
+  const config = parseLocalStorage();
+  config[key] = value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(config));
+};
+
+const loadLSUser = () => loadFromLocalStorage(USER_KEY);
+const saveLSUser = (value) => saveInLocalStorageAs(USER_KEY, value);
+
+const loadLSUserToken = () => loadFromLocalStorage(USER_TOKEN_KEY);
+const saveLSUserToken = (value) => saveInLocalStorageAs(USER_TOKEN_KEY, value);
+
+const initialToken = loadLSUserToken() || '';
 
 const initialState = {
-    isLoggedIn: false,
-    logInIsFetching: false,
-    userMail: 'test@mail.com',
-    userPass: '',
-    currentUser: '',
-    token: ''
-}
+  isLoggedIn: false,
+  logInIsFetching: false,
+  userMail: 'test@mail.com',
+  userPass: '',
+  currentUser: '',
+  token: initialToken
+};
 
 const actions = {
-    FETCH_LOG_IN        : 'FETCH_LOG_IN',
-    FETCH_LOG_IN_DONE   : 'FETCH_LOG_IN_DONE',
-    FETCH_LOG_IN_FAILED : 'FETCH_LOG_IN_FAILED',
-    LOG_OUT             : 'LOG_OUT',
-    CHANGE_USER_MAIL    : 'CHANGE_USER_MAIL',
-    CHANGE_USER_PASS    : 'CHANGE_USER_PASS'
-}
+  FETCH_LOG_IN: 'FETCH_LOG_IN',
+  FETCH_LOG_IN_DONE: 'FETCH_LOG_IN_DONE',
+  FETCH_LOG_IN_FAILED: 'FETCH_LOG_IN_FAILED',
+  LOG_OUT: 'LOG_OUT',
+  CHANGE_USER_MAIL: 'CHANGE_USER_MAIL',
+  CHANGE_USER_PASS: 'CHANGE_USER_PASS'
+};
 
 const loginReducer = (state = initialState, action) => {
-    const {type, payload = {}} = action;
-    const reducers = {
-        [actions.FETCH_LOG_IN]: () => ({
-            ...state,
-            isLoggedIn: false,
-            logInIsFetching: true,
-            currentUser: initialState.currentUser,
-            token: initialState.token
-        }),
-        [actions.FETCH_LOG_IN_DONE]: () => ({
-            ...state,
-            isLoggedIn: true,
-            logInIsFetching: false,
-            userMail: initialState.userMail,
-            userPass: initialState.userPass,
-            currentUser: payload.user,
-            token: payload.token
-        }),
-        [actions.FETCH_LOG_IN_FAILED]: () => ({
-            ...state,
-            isLoggedIn: false,
-            logInIsFetching: false
-        }),
-        [actions.LOG_OUT]: () => ({
-            ...state,
-            ...initialState
-        }),
-        [actions.CHANGE_USER_MAIL]: () => ({
-            ...state,
-            userMail: payload.value
-        }),
-        [actions.CHANGE_USER_PASS]: () => ({
-            ...state,
-            userPass: payload.value
+  const { type, payload = {} } = action;
+  const reducers = {
+    [actions.FETCH_LOG_IN]: () => ({
+      ...state,
+      isLoggedIn: false,
+      logInIsFetching: true,
+      currentUser: initialState.currentUser,
+      token: initialState.token
+    }),
+    [actions.FETCH_LOG_IN_DONE]: () => ({
+      ...state,
+      isLoggedIn: true,
+      logInIsFetching: false,
+      userMail: initialState.userMail,
+      userPass: initialState.userPass,
+      currentUser: payload.user,
+      token: payload.token
+    }),
+    [actions.FETCH_LOG_IN_FAILED]: () => ({
+      ...state,
+      isLoggedIn: false,
+      logInIsFetching: false
+    }),
+    [actions.LOG_OUT]: () => ({
+      ...state,
+      ...initialState
+    }),
+    [actions.CHANGE_USER_MAIL]: () => ({
+      ...state,
+      userMail: payload.value
+    }),
+    [actions.CHANGE_USER_PASS]: () => ({
+      ...state,
+      userPass: payload.value
+    })
+  };
+
+  return reducers[type] ? reducers[type]() : state;
+};
+
+const LogInManager = (props) => {
+  const { children, ...otherProps } = props;
+  const [state, dispatch] = useReducer(loginReducer, initialState);
+
+  const onLogInDone = (email) => (res = {}) => {
+    const { data } = res;
+    const user = email;
+    const token = data.token.split('Bearer ')[1];
+    saveLSUser(user);
+    saveLSUserToken(token);
+    data && data.success
+      ? dispatch({
+          type: actions.FETCH_LOG_IN_DONE,
+          payload: { user, token }
         })
-    }
-
-    return reducers[type] ? reducers[type]() : state;
-}
-
-const LogInManager = props => {
-    const {children, ...otherProps} = props;
-    const [state, dispatch] = useReducer(loginReducer, initialState);
-
-    const onLogInDone = email => (res = {}) => {
-        const {data} = res;
-        data && data.success ?
-            dispatch({
-                type: actions.FETCH_LOG_IN_DONE,
-                payload: { user: email, token: data.token.split('Bearer ')[1]}
-            }) :
-            dispatch({
-                type: actions.FETCH_LOG_IN_FAILED,
-                payload: {}
-            });
-    }
-
-    const onLogInFailed = () => {
-        dispatch({
-            type: actions.FETCH_LOG_IN_FAILED,
-            payload: {}
+      : dispatch({
+          type: actions.FETCH_LOG_IN_FAILED,
+          payload: {}
         });
-    }
+  };
 
-    const doLogIn = () => {
-        const {userMail, userPass} = state;
-        dispatch({
-            type: actions.FETCH_LOG_IN,
-            payload: {}
+  const onLogInFailed = () => {
+    dispatch({
+      type: actions.FETCH_LOG_IN_FAILED,
+      payload: {}
+    });
+  };
+
+  const doLogIn = () => {
+    const { userMail, userPass } = state;
+    dispatch({
+      type: actions.FETCH_LOG_IN,
+      payload: {}
+    });
+    axios
+      .post('/api/users/login', {
+        email: userMail,
+        password: userPass
+      })
+      .then(onLogInDone(userMail))
+      .catch(onLogInFailed);
+  };
+
+  const logInFromStorage = () => {
+    const user = loadLSUser();
+    const token = loadLSUserToken();
+    dispatch({
+      type: actions.FETCH_LOG_IN,
+      payload: {}
+    });
+    axios
+      .get(`/api/users/current?user=${user}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then(onLoginFromStorageDone(user, token))
+      .catch(onLogInFailed);
+  };
+
+  const onLoginFromStorageDone = (user, token) => (res = {}) => {
+    const { data } = res;
+    data && data.success
+      ? dispatch({
+          type: actions.FETCH_LOG_IN_DONE,
+          payload: { user, token }
+        })
+      : dispatch({
+          type: actions.FETCH_LOG_IN_FAILED,
+          payload: {}
         });
-        axios.post('/api/users/login', {
-            email: userMail,
-            password: userPass
-        }).then(onLogInDone(userMail)).catch(onLogInFailed);
-    }
+  };
 
-    const logIn = doLogIn;
+  const logIn = doLogIn;
 
-    const logOut = () => dispatch({type: actions.LOG_OUT, payload: {}});
+  const logOut = () => dispatch({ type: actions.LOG_OUT, payload: {} });
 
-    const onUserMailChange = newValue => dispatch({
-        type: actions.CHANGE_USER_MAIL,
-        payload: {
-            value: newValue
-        }
+  const onUserMailChange = (newValue) =>
+    dispatch({
+      type: actions.CHANGE_USER_MAIL,
+      payload: {
+        value: newValue
+      }
     });
 
-    const onUserPassChange = newValue => dispatch({
-        type: actions.CHANGE_USER_PASS,
-        payload: {
-            value: newValue
-        }
+  const onUserPassChange = (newValue) =>
+    dispatch({
+      type: actions.CHANGE_USER_PASS,
+      payload: {
+        value: newValue
+      }
     });
 
-    const userInfoForProvider = {
-        user: state.currentUser,
-        token: state.token
-    }
+  const userInfoForProvider = {
+    user: state.currentUser,
+    token: state.token
+  };
 
-    return (
-        <UserProvider value={userInfoForProvider}>
-            {children({
-                ...otherProps,
-                ...state,
-                logIn,
-                logOut,
-                onUserMailChange,
-                onUserPassChange
-            })}
-        </UserProvider>
-    );
-}
+  return (
+    <UserProvider value={userInfoForProvider}>
+      {children({
+        ...otherProps,
+        ...state,
+        logInFromStorage,
+        logIn,
+        logOut,
+        onUserMailChange,
+        onUserPassChange
+      })}
+    </UserProvider>
+  );
+};
 
 export default LogInManager;
--- admin-client/src/appHeader/AppHeader.jsx
+++ admin-client/src/appHeader/AppHeader.jsx
@@ -1,85 +1,66 @@
 import React from 'react';
 import Container from '@luisgilgb/react-container';
-import {UserConsumer} from '../UserContext';
+import { UserConsumer } from '../UserContext';
 import './AppHeader.css';
 
-const AppHeader = props => {
-    const {
-        goToHome,
-        logInIsFetching,
-        userMail,
-        userPass,
-        logIn,
-        logOut,
-        onUserMailChange,
-        onUserPassChange,
-        ...otherProps
-    } = props;
+const AppHeader = (props) => {
+  const {
+    goToHome,
+    logInIsFetching,
+    userMail,
+    userPass,
+    logIn,
+    logOut,
+    onUserMailChange,
+    onUserPassChange,
+    ...otherProps
+  } = props;
 
-    return (
-        <header
-            className="app-header"
-        >
-            <div
-                className="home-btn"
-                onClick={goToHome}
-            >
-                ADMIN
-            </div>
-            <Container
-                className="header-toolbar"
-            >
-                <UserConsumer
-                    {...otherProps}
-                >
-                    {({user}) => user ? (
-                        <Container
-                            className="user-info"
-                        >
-                            <div
-                                className="user-name"
-                            >
-                                {user}
-                            </div>
-                            <button
-                                className="logout-btn"
-                                onClick={logOut}
-                            >
-                                Log out
-                            </button>
-                        </Container>) : (
-                        <form
-                            className="login-form"
-                            onSubmit={(e) => {
-                                e.preventDefault();
-                                logIn();
-                            }}
-                        >
-                            <input
-                                className="login-input"
-                                type="email"
-                                placeholder="e-mail"
-                                value={userMail}
-                                onChange={({target}) => onUserMailChange(target.value)}
-                            />
-                            <input
-                                className="login-input"
-                                type="password"
-                                placeholder="password"
-                                value={userPass}
-                                onChange={({target}) => onUserPassChange(target.value)}
-                            />
-                            <input
-                                className="login-submit"
-                                type="submit"
-                                value="Log in"
-                            />
-                        </form>
-                    )}
-                </UserConsumer>
-            </Container>
-        </header>
-    );
-}
+  return (
+    <header className="app-header">
+      <div className="home-btn" onClick={goToHome}>
+        ADMIN
+      </div>
+      <Container className="header-toolbar">
+        <UserConsumer {...otherProps}>
+          {({ user }) =>
+            user ? (
+              <Container className="user-info">
+                <div className="user-name">{user}</div>
+                <button className="logout-btn" onClick={logOut}>
+                  Log out
+                </button>
+              </Container>
+            ) : (
+              <form
+                className="login-form"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  logIn();
+                }}
+              >
+                <input
+                  className="login-input"
+                  type="email"
+                  placeholder="e-mail"
+                  value={userMail}
+                  onChange={({ target }) => onUserMailChange(target.value)}
+                />
+                <input
+                  className="login-input"
+                  type="password"
+                  placeholder="password"
+                  value={userPass}
+                  onChange={({ target }) => onUserPassChange(target.value)}
+                />
+                <input className="login-submit" type="submit" value="Log in" />
+              </form>
+            )
+          }
+        </UserConsumer>
+      </Container>
+    </header>
+  );
+};
 
 export default AppHeader;

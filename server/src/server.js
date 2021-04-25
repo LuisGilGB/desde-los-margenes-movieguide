abba0110fba471ba68512793e60b145336791d13
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2021-04-25 22:39:15 +0200

    Updated mongoose connect configuration to prevent deprecation warnings

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const countriesRoutes = require('./routes/api/countries');
-const peopleRoutes = require('./routes/api/people');
-const moviesRoutes = require('./routes/api/movies');
-const usersRoutes = require('./routes/api/users');
+const countriesRoutes = require("./routes/api/countries");
+const peopleRoutes = require("./routes/api/people");
+const moviesRoutes = require("./routes/api/movies");
+const usersRoutes = require("./routes/api/users");
 
-const bffPeopleRoutes = require('./routes/bff-api/people');
-const bffMoviesRoutes = require('./routes/bff-api/movies');
+const bffPeopleRoutes = require("./routes/bff-api/people");
+const bffMoviesRoutes = require("./routes/bff-api/movies");
 
 const app = express();
 
@@ -21,24 +21,29 @@ const DB = process.env.MONGO_URI;
 
 // Connect to MongoDB
 mongoose
-	.connect(DB)
-	.then(() => console.log('MongoDB connected'))
-	.catch(err => console.log(err));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/countries', countriesRoutes);
-app.use('/api/people', peopleRoutes);
-app.use('/api/movies', moviesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/countries", countriesRoutes);
+app.use("/api/people", peopleRoutes);
+app.use("/api/movies", moviesRoutes);
+app.use("/api/users", usersRoutes);
 // Use BFF Routes
-app.use('/bff/people', bffPeopleRoutes);
-app.use('/bff/movies', bffMoviesRoutes);
+app.use("/bff/people", bffPeopleRoutes);
+app.use("/bff/movies", bffMoviesRoutes);
 
 // Port
 const port = process.env.PORT || 5000;

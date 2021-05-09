02198892c53cc3a32f4b60e7fd87fc8d228fec8a
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2021-05-09 16:10:36 +0200

    Input component improvements

--- admin-client/src/components/inputs/Input.jsx
+++ admin-client/src/components/inputs/Input.jsx
@@ -6,6 +6,7 @@ const DEFAULT_CLASS_NAME =
   'appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm';
 
 const Input = ({
+  value,
   name,
   type,
   autoComplete,
@@ -13,8 +14,10 @@ const Input = ({
   className,
   placeholder,
   'data-testid': dataTestId,
+  onChange,
 }) => (
   <input
+    value={value}
     name={name}
     type={type}
     autoComplete={autoComplete}
@@ -22,10 +25,12 @@ const Input = ({
     className={classnames(DEFAULT_CLASS_NAME, className)}
     placeholder={placeholder}
     data-testid={dataTestId}
+    onChange={onChange}
   />
 );
 
 Input.propTypes = {
+  value: PropTypes.string,
   name: PropTypes.string,
   type: PropTypes.string,
   autoComplete: PropTypes.string,
@@ -33,9 +38,11 @@ Input.propTypes = {
   className: PropTypes.string,
   placeholder: PropTypes.string,
   'data-testid': PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 Input.defaultProps = {
+  value: '',
   name: null,
   type: 'text',
   autoComplete: 'off',
@@ -43,6 +50,7 @@ Input.defaultProps = {
   className: '',
   placeholder: '',
   'data-testid': '',
+  onChange: null,
 };
 
 export default Input;
